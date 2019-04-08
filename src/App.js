ec5ea6cb687cd52930e2521fb83c641ab522f480
Gavriel R-H <gavriel@gavrielrh.com>
2019-04-08 10:54:39 -0400

    Hook up react to the muse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { zipSamples, MuseClient } from 'muse-js';
+import { powerByBand, epoch, fft } from '@neurosity/pipes';
 
 class App extends Component {
+  state = {
+    status: false,
+    data: {}
+  };
+
+  subscribeToMuse = async () => {
+    try {
+      const client = new MuseClient();
+
+      client.connectionStatus.subscribe(status => {
+        this.setState({ status });
+        console.log(status ? 'Connected!' : 'Disconnected');
+      });
+
+      await client.connect();
+      await client.start();
+
+      zipSamples(client.eegReadings)
+        .pipe(
+          epoch({ duration: 1024, interval: 100, samplingRate: 256 }),
+          fft({ bins: 256 }),
+          powerByBand()
+        )
+        .subscribe(data => {
+          this.setState({ data });
+        });
+    } catch (err) {
+      console.error('Connection failed', err);
+    }
+  };
+
   render() {
+    const { status } = this.state;
+    const statusText = status ? 'Connected' : 'Disconnected';
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.subscribeToMuse} disabled={status}>
+          Connect
+        </button>
+        <div>Status: {statusText}</div>
       </div>
     );
   }
