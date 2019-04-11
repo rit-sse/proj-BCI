e1c5dce962f8d67bb088ef2401a1ae1f58c53084
Gavriel R-H <gavriel@gavrielrh.com>
2019-04-11 15:44:20 -0400

    Implement hackertyper, add select

---
+++ src/HackerTyper.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Scroll from 'react-scroll';
+import file from './kernel.txt';
+const scroll = Scroll.animateScroll;
+
+class HackerTyper extends React.Component {
+  state = {
+    fullText: [],
+    textIdx: 0,
+    text: ''
+  };
+
+  updateText = e => {
+    const { text, fullText, textIdx } = this.state;
+    const { speed } = this.props;
+
+    this.setState(
+      {
+        text: text + fullText.slice(textIdx, textIdx + speed).join(''),
+        textIdx: textIdx + speed
+      },
+      scroll.scrollToBottom({ duration: 0 })
+    );
+
+    document.title = `Speed: ${speed}`;
+  };
+
+  componentDidMount() {
+    fetch(file)
+      .then(r => r.text())
+      .then(text => this.setState({ fullText: text.split('') }));
+    document.addEventListener('keypress', this.updateText, false);
+    //this.interval = setInterval(this.updateText, 100);
+    document.body.style = 'background: black;';
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.updateText, false);
+    //clearInterval(this.interval);
+  }
+
+  render() {
+    const { text } = this.state;
+    return (
+      <div>
+        <pre style={{ fontSize: 20, color: 'green' }}>{text}</pre>
+      </div>
+    );
+  }
+}
+
+export default HackerTyper;
