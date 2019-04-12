37e947dc0323feab0dbf98f4b6ef5e567406c161
dquach217 <dq6180@rit.edu>
2019-04-12 12:35:32 -0400

    fidget spinner is now spinning forever

--- src/FidgetSpinner.js
+++ src/FidgetSpinner.js
@@ -11,14 +11,15 @@ const rotate = keyframes`
     transform: rotate(360deg);
   }
 `;
+
 const animation = props =>
   css`
-    ${rotate} ${props.speed}s infinite alternate;
+    ${rotate} ${props.speed}s linear infinite;
   `
 
   const Rotate = styled.div`
   display: inline-block;
-  animation: ${animation};
+  animation: ${props => animation(props)};
   padding: 2rem 1rem;
   font-size: 1.2rem;
 `;  
@@ -32,7 +33,7 @@ class FidgetSpinner extends React.Component{
     render(){
       return(
         <div>
-          <Rotate> <img src={img}/> </Rotate>
+          <Rotate speed = {this.props.speed}> <img src={img}/> </Rotate>
         </div>
       )
     };
