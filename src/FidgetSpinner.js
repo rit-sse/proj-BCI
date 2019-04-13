f50b87f0f6fafcf618e3807b3aff9c05a674fca6
dquach217 <dq6180@rit.edu>
2019-04-13 15:55:12 -0400

    add some clean changes to fidget spinner, lint issues resolved

--- src/FidgetSpinner.js
+++ src/FidgetSpinner.js
@@ -11,29 +11,22 @@ const rotate = keyframes`
     transform: rotate(360deg);
   }
 `;
-
 const animation = props =>
   css`
     ${rotate} ${props.speed}s linear infinite;
-  `
-
+  `;
   const Rotate = styled.div`
-  display: inline-block;
-  animation: ${props => animation(props)};
-  padding: 2rem 1rem;
-  font-size: 1.2rem;
+    display: inline-block;
+    animation: ${props => animation(props)};
+    padding: 2rem 1rem;
+    font-size: 1.2rem;
 `;  
 
 class FidgetSpinner extends React.Component{
-
-  constructor(props){
-      super(props);
-  };
-
     render(){
       return(
         <div>
-          <Rotate speed = {this.props.speed}> <img src={img}/> </Rotate>
+          <Rotate speed = {this.props.speed}> <img src={img} alt="Fidget Spinner"/> </Rotate>
         </div>
       )
     };
